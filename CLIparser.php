0522d5b39e28b170c7ced4e5c7aad5a506c30eae
ArdiGG <arkadiok2033@gmail.com>
2021-12-18 17:59:53 +0200

    Working version without file substituion

--- CLIparser.php
+++ CLIparser.php
@@ -1,24 +1,50 @@
 <?php
 
-class Parser
-{
-    public function parse($url)
-    {
-
-
-    }
-}
-
 interface Parse
 {
     public function parse($url);
 }
 
-class LinkParser implements Parse
+class Parser
 {
+
     public function parse($url)
     {
         $domain = parse_url($url)['host'];
+        $parser = new ImageParser(ImageCsvSave::getInstance(),$domain);
+        $parser->parse($url);
+        print_r("file:///home/arkadiy/PhpstormProjects/Parser/$domain.csv\n");
+    }
+
+    public function report($url)
+    {
+        $parseUrl = parse_url($url);
+        $domain = $parseUrl['host'] ?? $parseUrl['path'];
+        print_r($domain);
+        $file = file_get_contents('CSV/'.$domain.'.csv');
+        print_r($file);
+    }
+
+    public function help()
+    {
+        print_r("parse - запускает парсер, принимает обязательный параметр url (как с протоколом, так и без).\nКоманда report - выводит в консоль результаты анализа для домена, принимает обязательный параметр domain (как с протоколом, так и без).\nКоманда help - выводит список команд с пояснениями.\n");
+    }
+}
+
+class LinkParser implements Parse
+{
+    public $links = [];
+    public Parse $imgParser;
+    public static $checkedLinks = [];
+    public static $domain;
+    public function __construct($domain)
+    {
+        $this->imgParser = new ImageParser(ImageCsvSave::getInstance(),$domain);
+        self::$domain = $domain;
+    }
+
+    public function parse($url)
+    {
 
         $file = file_get_contents($url);
 
@@ -26,47 +52,63 @@ class LinkParser implements Parse
 
 
         for ($i = 0; $i < $urls; $i++) {
-            $pos = strpos($result[1][$i], $domain);
+            $pos = strpos($result[1][$i],self::$domain);
             if ($pos !== false) {
-                echo $result[1][$i] . "\n";
+                $this->links[] = $result[1][$i];
             }
 
         }
+        $this->parseImgByLinks($url);
+    }
 
+    public function parseImgByLinks($url)
+    {
+        foreach ($this->links as $link) {
+            if (in_array($link, self::$checkedLinks)) {
+                continue;
+            } else {
+                self::$checkedLinks[] = $link;
+                $this->imgParser->parse($link,self::$domain);
+            }
+        }
     }
 }
 
-interface Data{
-    public function create();
+interface Saver
+{
+    public function save($data,$domain);
 }
 
-class ImageCsvSave{
+class ImageCsvSave implements Saver
+{
     public static ImageCsvSave $imageCsvSaveInstance;
 
     public static function getInstance()
     {
-        if(!isset(self::$imageCsvSaveInstance)){
+        if (!isset(self::$imageCsvSaveInstance)) {
             self::$imageCsvSaveInstance = new ImageCsvSave();
         }
 
         return self::$imageCsvSaveInstance;
     }
-    
-    public function save($imgPath)
-    {
-        $fp = fopen('results.csv', 'w');
 
-        fputcsv($fp, $imgPath);
+    public function save($data,$domain)
+    {
+
+        $fp = fopen("CSV/".$domain.'.csv', 'a');
+
+        fputcsv($fp, $data);
     }
 }
 
 class ImageParser implements Parse
 {
-    public ImageCsvSave $imgCsvSave;
-
-    public function __construct(ImageCsvSave $imgCsvSave)
+    public Saver $imgUrlSave;
+    public static $domain;
+    public function __construct(Saver $imgUrlSave,$domain)
     {
-        $this->imgCsvSave = $imgCsvSave;
+        $this->imgUrlSave = $imgUrlSave;
+        self::$domain = $domain;
     }
 
     public function parse($url)
@@ -93,14 +135,17 @@ class ImageParser implements Parse
             }
         }
 
-        $this->imgCsvSave->save($imgPath);
+        $this->imgUrlSave->save($imgPath,self::$domain);
 
-        print_r("file:///home/arkadiy/PhpstormProjects/Parser/results.csv\n");
+
+        $linkParser = new LinkParser(self::$domain);
+        $linkParser->parse($url);
     }
 }
 
+$parser = new Parser();
 
+$action = $argv[1];
+$parser->$action($argv[2]);
 
-$parser = new ImageParser(ImageCsvSave::getInstance());
-$parser->parse($argv[1]);
 
